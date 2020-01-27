7d0c01be4af5429181cc91e377a2e4bc5886b08a
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-01-27 12:10:36 -0800

    commenting, cleanup

--- tools/sling.lua
+++ tools/sling.lua
@@ -17,22 +17,13 @@ registerCoreBehavior(SlingTool)
 
 
 local MAX_SPEED = 8 * UNIT
+
 local SPEED_MULTIPLIER = 2
 local DRAW_MULTIPLIER = 0.6
-local CIRCLE_DRAW_RADIUS = 0.25 * UNIT
-local TRIANGLE_DRAW_LENGTH = 0.25 * UNIT
-local TRIANGLE_DRAW_WIDTH = 0.1 * UNIT
 
-
--- Behavior management
-
-function SlingTool.handlers:addBehavior(opts)
-    self._gridEnabled = false
-    self._gridSize, self._gridSize = UNIT, UNIT
-
-    self._rotateIncrementEnabled = false
-    self._rotateIncrementDegrees = 45
-end
+local CIRCLE_RADIUS = 0.25 * UNIT
+local TRIANGLE_LENGTH = 0.25 * UNIT
+local TRIANGLE_WIDTH = 0.1 * UNIT
 
 
 -- Update
@@ -54,30 +45,32 @@ function SlingTool.handlers:update(dt)
         return
     end
 
-    local physics = self.dependencies.Body:getPhysics()
-
+    -- Look for a single-finger drag and release
     local touchData = self:getTouchData()
     if touchData.maxNumTouches == 1 and touchData.allTouchesReleased then
         local touchId, touch = next(touchData.touches)
 
         local vX, vY = touch.initialX - touch.x, touch.initialY - touch.y
-        if vX == 0 and vY == 0 then
+        if vX == 0 and vY == 0 then -- Release without moving touch? Just select.
             self.game:selectActorAtTouch(touch)
-        else
+        else -- Moved and released -- apply velocity
             local vLen = math.sqrt(vX * vX + vY * vY)
             if vLen > MAX_SPEED then
                 vX, vY = vX * MAX_SPEED / vLen, vY * MAX_SPEED / vLen
             end
 
+            local physics = self.dependencies.Body:getPhysics()
+
             for actorId, component in pairs(self.components) do
                 if self.game.clientId == component.clientId then
                     local bodyId, body = self.dependencies.Body:getBody(actorId)
+
+                    -- Send a message to set velocity, but also set one locally for smoothness
                     physics:setLinearVelocity({
                         selfSend = false,
                         forwardToOrigin = true,
                     }, bodyId, SPEED_MULTIPLIER * vX, SPEED_MULTIPLIER * vY)
-                    body:setLinearVelocity(
-                        0.5 * SPEED_MULTIPLIER * vX, 0.5 * SPEED_MULTIPLIER * vY)
+                    body:setLinearVelocity(0.5 * SPEED_MULTIPLIER * vX, 0.5 * SPEED_MULTIPLIER * vY)
                 end
             end
         end
@@ -92,31 +85,37 @@ function SlingTool.handlers:drawOverlay(dt)
         return
     end
 
+    -- Look for a single-finger drag
     local touchData = self:getTouchData()
     if touchData.numTouches == 1 then
         local touchId, touch = next(touchData.touches)
 
         local vX, vY = touch.initialX - touch.x, touch.initialY - touch.y
         local vLen = math.sqrt(vX * vX + vY * vY)
-        if vLen > MAX_SPEED then
-            vX, vY = vX * MAX_SPEED / vLen, vY * MAX_SPEED / vLen
-        end
+        if vLen > 0 then
+            if vLen > MAX_SPEED then
+                vX, vY = vX * MAX_SPEED / vLen, vY * MAX_SPEED / vLen
+            end
 
-        love.graphics.circle('line', touch.initialX, touch.initialY, CIRCLE_DRAW_RADIUS)
-        local r, g, b, a = love.graphics.getColor()
-        love.graphics.setColor(r, g, b, 0.5 * a)
-        love.graphics.circle('fill', touch.initialX, touch.initialY, CIRCLE_DRAW_RADIUS)
-        love.graphics.setColor(r, g, b, a)
-        local endX, endY = touch.initialX + DRAW_MULTIPLIER * vX, touch.initialY + DRAW_MULTIPLIER * vY
-        love.graphics.line(
-            touch.initialX, touch.initialY,
-            endX - TRIANGLE_DRAW_LENGTH * vX / vLen, endY - TRIANGLE_DRAW_LENGTH * vY / vLen)
-        love.graphics.polygon('fill',
-            endX, endY,
-            endX - TRIANGLE_DRAW_LENGTH * vX / vLen - TRIANGLE_DRAW_WIDTH * vY / vLen,
-            endY - TRIANGLE_DRAW_LENGTH * vY / vLen + TRIANGLE_DRAW_WIDTH * vX / vLen,
-            endX - TRIANGLE_DRAW_LENGTH * vX / vLen + TRIANGLE_DRAW_WIDTH * vY / vLen,
-            endY - TRIANGLE_DRAW_LENGTH * vY / vLen - TRIANGLE_DRAW_WIDTH * vX / vLen)
+            -- Circle with solid outline and transparent fill
+            love.graphics.circle('line', touch.initialX, touch.initialY, CIRCLE_RADIUS)
+            local r, g, b, a = love.graphics.getColor()
+            love.graphics.setColor(r, g, b, 0.5 * a)
+            love.graphics.circle('fill', touch.initialX, touch.initialY, CIRCLE_RADIUS)
+            love.graphics.setColor(r, g, b, a)
+
+            -- Line and triangle
+            local endX, endY = touch.initialX + DRAW_MULTIPLIER * vX, touch.initialY + DRAW_MULTIPLIER * vY
+            love.graphics.line(
+                touch.initialX, touch.initialY,
+                endX - TRIANGLE_LENGTH * vX / vLen, endY - TRIANGLE_LENGTH * vY / vLen)
+            love.graphics.polygon('fill',
+                endX, endY,
+                endX - TRIANGLE_LENGTH * vX / vLen - TRIANGLE_WIDTH * vY / vLen,
+                endY - TRIANGLE_LENGTH * vY / vLen + TRIANGLE_WIDTH * vX / vLen,
+                endX - TRIANGLE_LENGTH * vX / vLen + TRIANGLE_WIDTH * vY / vLen,
+                endY - TRIANGLE_LENGTH * vY / vLen - TRIANGLE_WIDTH * vX / vLen)
+        end
     end
 end
 
