de86f668ffe631d5ddbcdbf0458b7c2d376a99fc
Jesse Ruder <jesse@getexponent.com>
2020-05-28 19:23:12 -0700

    add curving to new drawing tool

--- tools/draw2.lua
+++ tools/draw2.lua
@@ -84,13 +84,144 @@ local function resetGraphics()
     _graphics:setDisplay("mesh", 1024)
 
     for i = 1, #_paths do
-        _graphics:addPath(_paths[i])
+        _graphics:addPath(_paths[i].path)
     end
 end
 
-local function drawSubpath(subpath, p1, p2)
-    subpath:moveTo(p1.x, p1.y)
-    subpath:lineTo(p2.x, p2.y)
+local function drawEndOfArc(path, p1x, p1y, p2x, p2y)
+    if p1x == p2x and p1y == p2y then
+        return
+    end
+
+    local subpath = tove.newSubpath()
+    path:addSubpath(subpath)
+    subpath:moveTo(p1x, p1y)
+    subpath:lineTo(p2x, p2y)
+end
+
+local function drawPath(pathData)
+    local path = pathData.path
+    local p1 = pathData.p1
+    local p2 = pathData.p2
+    local style = pathData.style
+
+    local subpath = tove.newSubpath()
+    path:addSubpath(subpath)
+
+    if style == 1 then
+        subpath:moveTo(p1.x, p1.y)
+        subpath:lineTo(p2.x, p2.y)
+        return
+    end
+
+    local isOver = style == 2
+
+    if p1.x > p2.x or (p1.x == p2.x and p1.y > p2.y) then
+        local t = p1
+        p1 = p2
+        p2 = t
+    end
+
+    local radius = math.min(math.abs(p2.x - p1.x), math.abs(p2.y - p1.y))
+    local xIsLonger = math.abs(p2.x - p1.x) > math.abs(p2.y - p1.y)
+
+    if radius == 0 then
+        subpath:moveTo(p1.x, p1.y)
+        subpath:lineTo(p2.x, p2.y)
+    else
+        local circleCenter = {}
+        local startAngle
+
+        if p1.y > p2.y then
+            startAngle = 0.0
+            if isOver then
+                startAngle = startAngle + 180.0
+            end
+
+            if xIsLonger then
+                --
+                --             .
+                -- .
+                --
+                if isOver then
+                    circleCenter.x = p1.x + radius
+                    circleCenter.y = p2.y + radius
+
+                    drawEndOfArc(path, p1.x + radius, p2.y, p2.x, p2.y)
+                else
+                    circleCenter.x = p2.x - radius
+                    circleCenter.y = p1.y - radius
+
+                    drawEndOfArc(path, p1.x, p1.y, p2.x - radius, p1.y)
+                end
+            else
+                --
+                --   .
+                --
+                --
+                --
+                -- .
+                --
+                if isOver then
+                    circleCenter.x = p1.x + radius
+                    circleCenter.y = p2.y + radius
+
+                    drawEndOfArc(path, p1.x, p1.y, p1.x, p2.y + radius)
+                else
+                    circleCenter.x = p2.x - radius
+                    circleCenter.y = p1.y - radius
+
+                    drawEndOfArc(path, p2.x, p1.y - radius, p2.x, p2.y)
+                end
+            end
+        else
+            startAngle = 90.0
+            if isOver then
+                startAngle = startAngle + 180.0
+            end
+
+            if xIsLonger then
+                --
+                -- .
+                --             .
+                --
+                if isOver then
+                    circleCenter.x = p2.x - radius
+                    circleCenter.y = p1.y + radius
+
+                    drawEndOfArc(path, p1.x, p1.y, p2.x - radius, p1.y)
+                else
+                    circleCenter.x = p1.x + radius
+                    circleCenter.y = p2.y - radius
+
+                    drawEndOfArc(path, p1.x + radius, p2.y, p2.x, p2.y)
+                end
+            else
+                --
+                -- .
+                --
+                --
+                --
+                --   .
+                --
+                if isOver then
+                    circleCenter.x = p2.x - radius
+                    circleCenter.y = p1.y + radius
+
+                    drawEndOfArc(path, p2.x, p1.y + radius, p2.x, p2.y)
+                else
+                    circleCenter.x = p1.x + radius
+                    circleCenter.y = p2.y - radius
+
+                    drawEndOfArc(path, p1.x, p1.y, p1.x, p2.y - radius)
+                end
+            end
+        end
+
+        --print('center:' .. circleCenter.x .. ', ' .. circleCenter.y .. ' radius:' .. radius .. ' startangle:' .. startAngle)
+        subpath:arc(circleCenter.x, circleCenter.y, radius, startAngle, startAngle + 90.0)
+        --subpath:arc(0.0, 0.0, 4.0, 0.0, 90.0)
+    end
 end
 
 function DrawTool:getSingleComponent()
@@ -112,6 +243,7 @@ end
 function DrawTool.handlers:onSetActive()
     _paths = {}
     _initialCoord = nil
+    _tempGraphics = nil
 
     resetGraphics()
 end
@@ -138,22 +270,47 @@ function DrawTool.handlers:preUpdate(dt)
             _initialCoord = roundedCoord
         end
 
-        local subpath = tove.newSubpath()
+        local pathData = {}
         local path = tove.newPath()
-        path:addSubpath(subpath)
 
         path:setLineColor(0.0, 0.0, 0.0, 1.0)
         path:setLineWidth(0.2)
         path:setMiterLimit(1)
         path:setLineJoin("round")
 
-        drawSubpath(subpath, _initialCoord, roundedCoord)
+        pathData.path = path
+        pathData.p1 = _initialCoord
+        pathData.p2 = roundedCoord
+        pathData.style = 1
+        drawPath(pathData)
 
         if touch.released then
-            addPath(path)
-            resetGraphics()
+            if pathData.p1.x == pathData.p2.x and pathData.p1.y == pathData.p2.y then
+                for i = 1, #_paths do
+                    if _paths[i].path:nearest(touch.x, touch.y, 0.5) then
+                        local path = tove.newPath()
+    
+                        path:setLineColor(0.0, 0.0, 0.0, 1.0)
+                        path:setLineWidth(0.2)
+                        path:setMiterLimit(1)
+                        path:setLineJoin("round")
+    
+                        _paths[i].path = path
+                        _paths[i].style = _paths[i].style + 1
+                        if _paths[i].style > 3 then
+                            _paths[i].style = 1
+                        end
+                        drawPath(_paths[i])
+                        resetGraphics()
+                    end
+                end
+            else
+                addPath(pathData)
+                resetGraphics()
+            end
 
             _initialCoord = nil
+            _tempGraphics = nil
         else
             _tempGraphics = tove.newGraphics()
             _tempGraphics:setDisplay("mesh", 1024)
