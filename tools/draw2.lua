31ef310bad5410a0cd4c6507973dcad2035d4030
Jesse Ruder <jesse@getexponent.com>
2020-05-29 13:57:59 -0700

    basic raster flood fill

--- tools/draw2.lua
+++ tools/draw2.lua
@@ -35,29 +35,238 @@ local GRID_TOP_PADDING = 0.2 * DEFAULT_VIEW_WIDTH
 local GRID_SIZE = 10
 local GRID_WIDTH = DEFAULT_VIEW_WIDTH - GRID_HORIZONTAL_PADDING * 2.0
 
+local pathsCanvas
 local fillCanvas
 
-function floodFill()
-    local windowWidth, windowHeight = love.graphics.getDimensions()
-    fillCanvas =
-    love.graphics.newCanvas(
-        windowWidth,
-        windowHeight,
-        {
-            dpiscale = 1,
-            msaa = 4
-        }
-    )
+-- http://will.thimbleby.net/scanline-flood-fill/
+function floodFillScanline(x, y, width, height, diagonal, test, paint)
+    -- xMin, xMax, y, down[true] / up[false], extendLeft, extendRight
+    local ranges = {}
+    table.insert(ranges, {
+        xMin = x,
+        xMax = x,
+        y = y,
+        direction = nil,
+        extendLeft = true,
+        extendRight = true,
+    })
 
-    fillCanvas:renderTo(
+    paint(x, y)
+
+    while #ranges > 0 do
+        local r = table.remove(ranges)
+        -- print(inspect(r))
+        local down = r.direction == 'down'
+        local up =   r.direction == 'up'
+
+        -- extendLeft
+        local minX = r.xMin
+        local y = r.y
+        if r.extendLeft then
+            while minX > 0 and test(minX-1, y) do
+                minX = minX - 1
+                paint(minX, y)
+            end
+        end
+
+        local maxX = r.xMax
+        -- extendRight
+        if r.extendRight then
+            while maxX < width - 1 and test(maxX+1, y) do
+                maxX = maxX + 1
+                paint(maxX, y)
+            end
+        end
+
+        if diagonal then
+            -- extend range looked at for next lines
+            if minX>0 then minX = minX - 1 end
+            if maxX<width-1 then maxX = maxX + 1 end
+        else
+            -- extend range ignored from previous line
+            r.xMin = r.xMin - 1
+            r.xMax = r.xMax + 1
+        end
+
+        local function addNextLine(newY, isNext, direction)
+            local rMinX = minX
+            local inRange = false
+            for x=minX, maxX do
+                -- skip testing, if testing previous line within previous range
+                local empty = (isNext or (x<r.xMin or x>r.xMax)) and test(x, newY)
+                if (not inRange) and empty then
+                    rMinX = x
+                    inRange = true
+                elseif inRange and (not empty) then
+                    table.insert(ranges, {
+                        xMin = rMinX,
+                        xMax = x - 1,
+                        y = newY,
+                        direction = direction,
+                        extendLeft = rMinX==minX,
+                        extendRight = false,
+                    })
+
+                    inRange = false
+                end
+
+                if inRange then
+                    paint(x, newY)
+                end
+                -- skip
+                if (not isNext) and x==r.xMin then
+                    x = r.xMax
+                end
+            end
+            if inRange then
+                table.insert(ranges, {
+                    xMin = rMinX,
+                    xMax = x - 1,
+                    y = newY,
+                    direction = direction,
+                    extendLeft = rMinX==minX,
+                    extendRight = true,
+                })
+            end
+        end
+
+        if(y<height-1) then
+            addNextLine(y+1, not up, 'down')
+        end
+        if(y>0) then
+            addNextLine(y-1, not down, 'up')
+        end
+    end
+end
+
+function floodFill8Way(startX, startY, width, height, test, paint)
+    local queue = {}
+
+    if startX < 0 or startX < 0 or startX >= width or startX >= height then
+        return
+    end
+
+    table.insert(queue, {
+        x = startX,
+        y = startY,
+    })
+
+    while #queue > 0 do
+        local item = table.remove(queue)
+        local x = item.x
+        local y = item.y
+
+        if test(x, y) then
+            paint(x, y)
+
+            for dx = -1, 1 do
+                for dy = -1, 1 do
+                    local skip = false
+                    if dx == 0 and dy == 0 then
+                        skip = true
+                    end
+
+                    local newX = x + dx
+                    local newY = y + dy
+
+                    if newX < 0 or newY < 0 or newX >= width or newY >= height then
+                        skip = true
+                    end
+
+                    if not skip then
+                        if test(newX, newY) then
+                            table.insert(queue, {
+                                x = newX,
+                                y = newY,
+                            })
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function floodFill(x, y)
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+
+    pathsCanvas:renderTo(
         function()
             love.graphics.push("all")
 
             love.graphics.origin()
             love.graphics.scale(windowWidth / DEFAULT_VIEW_WIDTH)
 
+            love.graphics.clear(0, 0, 0, 0)
             love.graphics.setColor(1, 1, 1, 1)
             _graphics:draw()
+
+            love.graphics.pop()
+        end
+    )
+
+    local pathsImageData = pathsCanvas:newImageData()
+    local fillImageData = fillCanvas:newImageData()
+
+    fillCanvas:renderTo(
+        function()
+            love.graphics.push("all")
+
+            love.graphics.origin()
+            --love.graphics.scale(windowWidth / DEFAULT_VIEW_WIDTH)
+
+            love.graphics.setColor(0.5, 0.5, 0.5, 1)
+            
+            love.graphics.setPointSize(10.0)
+
+            local points = {}
+            local lookup = {}
+
+            local scale = windowWidth / DEFAULT_VIEW_WIDTH
+
+            local function test(x, y)
+                if lookup[x] and lookup[x][y] then
+                    return false
+                end
+
+                if x < scale * GRID_HORIZONTAL_PADDING or x > scale * (GRID_HORIZONTAL_PADDING + GRID_WIDTH) then
+                    return false
+                end
+
+                if y < scale * GRID_TOP_PADDING or y > scale * (GRID_TOP_PADDING + GRID_WIDTH) then
+                    return false
+                end
+
+                if x < 0 or y < 0 or x >= pathsImageData:getWidth() or y >= pathsImageData:getHeight() then
+                    return false
+                end
+
+                local r, g, b, a = pathsImageData:getPixel(x, y)
+                if a > 0.0 then
+                    return false
+                end
+
+                r, g, b, a = fillImageData:getPixel(x, y)
+                if a > 0.0 then
+                    return false
+                end
+
+                return true
+            end
+
+            local function paint(x, y)
+                table.insert(points, x)
+                table.insert(points, y)
+
+                if lookup[x] == nil then
+                    lookup[x] = {}
+                end
+                lookup[x][y] = true
+            end
+
+            floodFill8Way(math.floor(x * scale), math.floor(y * scale), windowWidth, windowHeight, test, paint)
+
+            love.graphics.points(points)
 
             love.graphics.pop()
         end
@@ -276,6 +485,26 @@ function DrawTool.handlers:onSetActive()
     _tempGraphics = nil
 
     resetGraphics()
+
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    pathsCanvas =
+    love.graphics.newCanvas(
+        windowWidth,
+        windowHeight,
+        {
+            dpiscale = 1,
+            msaa = 4
+        }
+    )
+    fillCanvas =
+    love.graphics.newCanvas(
+        windowWidth,
+        windowHeight,
+        {
+            dpiscale = 1,
+            msaa = 4
+        }
+    )
 end
 
 function DrawTool.handlers:preUpdate(dt)
@@ -341,7 +570,7 @@ function DrawTool.handlers:preUpdate(dt)
                 end
 
                 if not foundPath then
-                    floodFill()
+                    floodFill(touch.x, touch.y)
                 end
             else
                 addPath(pathData)
@@ -402,8 +631,7 @@ function DrawTool.handlers:drawOverlay()
         love.graphics.draw(fillCanvas, 0, 0, 0, DEFAULT_VIEW_WIDTH / windowWidth, DEFAULT_VIEW_WIDTH / windowWidth)
     end
 
-    --_graphics:draw()
-
+    _graphics:draw()
 
     if _tempGraphics ~= nil then
         _tempGraphics:draw()
