3b9057d6bb32b87f14e7219bc7c56a7d491cf3c9
Ben Roth <spongebucket@gmail.com>
2020-06-05 13:32:05 -0700

    breaking out add/remove behavior, save blueprint

--- ui/blueprints.lua
+++ ui/blueprints.lua
@@ -1,3 +1,136 @@
+function Client:_saveBlueprintButton(actor)
+    ui.button('save blueprint', {
+        flex = 1,
+        icon = 'save',
+        iconFamily = 'FontAwesome5',
+        onClick = function()
+            self.saveBlueprintDatas[actor] = nil
+        end,
+        popoverAllowed = true,
+        popoverStyle = { width = 300 },
+        popover = function(closePopover)
+            ui.scrollBox('save blueprint', { flex = 1 }, function()
+                local saveBlueprintData = self.saveBlueprintDatas[actor]
+                if not saveBlueprintData then
+                    local oldEntry = self.library[actor.parentEntryId]
+                    if oldEntry.isCore then
+                        -- Don't overwrite core entries
+                        oldEntry = nil
+                    end
+                    saveBlueprintData = {
+                        entryId = oldEntry and oldEntry.entryId or nil,
+                        title = oldEntry and oldEntry.title or '',
+                        description = oldEntry and oldEntry.description or '',
+                    }
+                    self.saveBlueprintDatas[actor] = saveBlueprintData
+                end
+
+                saveBlueprintData.title = ui.textInput('title', saveBlueprintData.title)
+
+                saveBlueprintData.description = ui.textArea('description', saveBlueprintData.description)
+
+                local existingEntry = self.library[saveBlueprintData.entryId]
+                if existingEntry then
+                    local numOtherActors = 0
+                    for otherActorId, otherActor in pairs(self.actors) do
+                        if otherActorId ~= actorId and otherActor.parentEntryId == existingEntry.entryId then
+                            numOtherActors = numOtherActors + 1
+                        end
+                    end
+                    if numOtherActors > 0 then
+                        ui.markdown('This blueprint is used by ' .. numOtherActors ..
+                            ' other actor' .. (numOtherActors > 1 and 's' or ''))
+                    else
+                        ui.markdown('No other actors use this blueprint')
+                    end
+
+                    ui.button('update used blueprint', {
+                        icon = 'file-upload',
+                        iconFamily = 'FontAwesome5',
+                        onClick = function()
+                            if saveBlueprintData.title == '' then
+                                castle.system.alert('Title required', 'Please enter a title for the blueprint.')
+                                return
+                            end
+                            for entryId, entry in pairs(self.library) do
+                                if (entry.entryType == 'actorBlueprint' and
+                                        entry.entryId ~= existingEntry.entryId and
+                                        entry.title == saveBlueprintData.title) then
+                                    castle.system.alert('Title in use',
+                                        'This title is already used by another blueprint. Please enter a different title.')
+                                    return
+                                end
+                            end
+
+                            local entryId = existingEntry.entryId
+                            local oldEntry = existingEntry
+                            local newActorBp = self:blueprintActor(actor.actorId)
+                            if newActorBp.components.Body then
+                                newActorBp.components.Body.x, newActorBp.components.Body.y = nil, nil
+                            end
+                            local newEntry = {
+                                entryType = 'actorBlueprint',
+                                title = saveBlueprintData.title,
+                                description = saveBlueprintData.description,
+                                actorBlueprint = newActorBp,
+                            }
+
+                            closePopover()
+
+                            self:command('update blueprint', {
+                                params = { 'entryId', 'oldEntry', 'newEntry' },
+                            }, function(params, live)
+                                self:send('updateLibraryEntry', self.clientId, entryId, newEntry, {
+                                    updateActors = true,
+                                    skipActorId = actorId,
+                                })
+                            end, function()
+                                self:send('updateLibraryEntry', self.clientId, entryId, oldEntry, {
+                                    updateActors = true,
+                                    skipActorId = actorId,
+                                })
+                            end)
+                        end,
+                    })
+                end
+
+                ui.button('save as new blueprint', {
+                    icon = 'addfile',
+                    iconFamily = 'AntDesign',
+                    onClick = function()
+                        if saveBlueprintData.title == '' then
+                            castle.system.alert('Title required', 'Please enter a title for the new blueprint.')
+                            return
+                        end
+                        for entryId, entry in pairs(self.library) do
+                            if entry.entryType == 'actorBlueprint' and entry.title == saveBlueprintData.title then
+                                castle.system.alert('Title in use',
+                                    'This title is already used by another blueprint. Please enter a different title.')
+                                return
+                            end
+                        end
+
+                        closePopover()
+
+                        local newEntryId = util.uuid()
+                        local newActorBp = self:blueprintActor(actor.actorId)
+                        if newActorBp.components.Body then
+                            newActorBp.components.Body.x, newActorBp.components.Body.y = nil, nil
+                        end
+                        self:send('addLibraryEntry', newEntryId, {
+                            entryType = 'actorBlueprint',
+                            title = saveBlueprintData.title,
+                            description = saveBlueprintData.description,
+                            actorBlueprint = newActorBp,
+                        })
+                        self:send('setActorParentEntryId', actor.actorId, newEntryId)
+                    end,
+                })
+            end)
+        end
+    })
+end
+
 function Client:uiBlueprints()
     self:uiLibrary({
         id = 'add actor',
