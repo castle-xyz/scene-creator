5d2cd9763359075cf4a433f1779dec61156a0340
Ben Roth <spongebucket@gmail.com>
2020-06-11 14:22:08 -0700

    data actions to add/remove behaviors with blueprint

--- ui/inspector.lua
+++ ui/inspector.lua
@@ -14,70 +14,53 @@ function Client:_uiActorAllowsBehavior(actor, behavior)
    return true
 end
 
-function Client:_removeBehaviorButton(actorId, component, behavior, uiName)
-    ui.button('remove', {
-        margin = 0,
-        marginLeft = 6,
-        icon = 'close',
-        iconFamily = 'FontAwesome',
-        hideLabel = true,
-        onClick = function()
-            if next(component.dependents) ~= nil then -- Has dependents?
-                local names = {}
-                for dependentId in pairs(component.dependents) do
-                    local dependent = self.behaviors[dependentId]
-                    table.insert(names, dependent:getUiName())
+function Client:_removeBehavior(actorId, component, behavior)
+    local uiName = behavior:getUiName()
+    if next(component.dependents) ~= nil then -- Has dependents?
+        local names = {}
+        for dependentId in pairs(component.dependents) do
+            local dependent = self.behaviors[dependentId]
+            table.insert(names, dependent:getUiName())
+        end
+        local list = ''
+        for i = 1, #names do
+            if i > 1 then
+                if i < #names then
+                    list = list .. ', '
+                else
+                    list = list .. ' and '
                 end
-                local list = ''
-                for i = 1, #names do
-                    if i > 1 then
-                        if i < #names then
-                            list = list .. ', '
-                        else
-                            list = list .. ' and '
-                        end
-                    end
-                    list = list .. "'" .. names[i] .. "'"
-                end
-                local message = (list .. ' need' .. (#names > 1 and '' or 's') .. " '" ..
-                    uiName .. "'.")
-                castle.system.alert("Cannot remove '" .. uiName .. "'", message)
-            else
-                castle.system.alert({
-                    title = 'Remove behavior?',
-                    message = "Remove '" .. uiName .. "' from this actor?",
-                    okLabel = 'Yes',
-                    cancelLabel = 'No',
-                    onOk = function()
-                        local behaviorId = component.behaviorId
-                        local componentBp = {}
-                        behavior:callHandler('blueprintComponent', component, componentBp)
-                        self:command('remove ' .. uiName, {
-                            params = { 'behaviorId', 'componentBp' },
-                        }, function()
-                            local behavior = self.behaviors[behaviorId]
-                            if not behavior.components[actorId] then
-                                return 'behavior was removed'
-                            end
-                            self:send('removeComponent', self.clientId, actorId, behaviorId)
-                            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
-                        end, function()
-                            local behavior = self.behaviors[behaviorId]
-                            if behavior.components[actorId] then
-                                return 'behavior was added'
-                            end
-                            self:send('addComponent', self.clientId, actorId, behaviorId, componentBp)
-                            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
-                        end)
-                    end
-                })
             end
-        end,
-    })
+            list = list .. "'" .. names[i] .. "'"
+        end
+        local message = (list .. ' need' .. (#names > 1 and '' or 's') .. " '" ..
+            uiName .. "'.")
+        castle.system.alert("Cannot remove '" .. uiName .. "'", message)
+    else
+        local behaviorId = component.behaviorId
+        local componentBp = {}
+        behavior:callHandler('blueprintComponent', component, componentBp)
+        self:command('remove ' .. uiName, {
+            params = { 'behaviorId', 'componentBp' },
+        }, function()
+            local behavior = self.behaviors[behaviorId]
+            if not behavior.components[actorId] then
+                return 'behavior was removed'
+            end
+            self:send('removeComponent', self.clientId, actorId, behaviorId)
+            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
+        end, function()
+            local behavior = self.behaviors[behaviorId]
+            if behavior.components[actorId] then
+                return 'behavior was added'
+            end
+            self:send('addComponent', self.clientId, actorId, behaviorId, componentBp)
+            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
+        end)
+    end
 end
 
-function Client:_addBehavior(actor, behaviorId, closePopover)
-    closePopover = closePopover or function() end
+function Client:_addBehavior(actor, behaviorId, blueprint)
     local behavior = self.behaviors[behaviorId]
     local actorId = actor.actorId
 
@@ -98,100 +81,30 @@ function Client:_addBehavior(actor, behaviorId, closePopover)
     end
     visit(behavior)
 
-    -- Prompt if adding more than one behavior, else add immediately
-    local function doIt()
-        closePopover()
-
-        self:command('add ' .. behavior:getUiName(), {
-            params = { 'behaviorId', 'order' },
-        }, function()
-            local behavior = self.behaviors[behaviorId]
-            if behavior.components[actorId] then
-                return 'behavior was added'
-            end
-            for i = 1, #order do
-                self:send('addComponent', self.clientId, actorId, order[i], {}, {
-                    interactive = true,
-                })
-            end
-            self.openComponentBehaviorId = behaviorId
-            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
-        end, function()
-            local behavior = self.behaviors[behaviorId]
-            if not behavior.components[actorId] then
-                return 'behavior was removed'
-            end
-            for i = #order, 1, -1 do
-                self:send('removeComponent', self.clientId, actorId, order[i])
-            end
-            self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
-        end)
-    end
-    if #order > 1 then
-        local list = ''
-        for i = 1, #order - 1 do
-            if i > 1 then
-                if i < #order - 1 then
-                    list = list .. ', '
-                else
-                    list = list .. ' and '
-                end
-            end
-            list = list .. "'" .. self.behaviors[order[i]]:getUiName() .. "'"
+    self:command('add ' .. behavior:getUiName(), {
+        params = { 'behaviorId', 'order', 'blueprint' },
+    }, function()
+        local behavior = self.behaviors[behaviorId]
+        if behavior.components[actorId] then
+            return 'behavior was added'
         end
-        local message = ("'" .. behavior:getUiName() .. "' needs " .. list ..
-            '. Add ' .. (#order == 2 and 'it' or 'them') .. ' also?')
-        castle.system.alert({
-            title = 'Add needed behaviors?',
-            message = message,
-            okLabel = 'Yes',
-            cancelLabel = 'No',
-            onOk = function()
-                doIt()
-            end,
-            onCancel = function()
-                closePopover()
-            end
-        })
-    else
-        doIt()
-    end
-end
-
-function Client:_addBehaviorButton(actor)
-    ui.button('add behavior', {
-        icon = 'plus',
-        iconFamily = 'FontAwesome5',
-        hideLabel = true,
-        popoverAllowed = true,
-        popoverStyle = { width = 300, height = 300 },
-        popover = function(closePopover)
-            self:uiLibrary({
-                id = 'add behavior',
-                filterType = 'behavior',
-                filterBehavior = function(behavior)
-                   -- skip behavior limitations enforced at ui level
-                   if not self:_uiActorAllowsBehavior(actor, behavior) then return false end
-
-                   -- Skip behaviors we already have, skip tools
-                    return not (actor.components[behavior.behaviorId] or behavior.tool)
-                end,
-                emptyText = 'No other behaviors to add!',
-                buttons = function(entry)
-                    ui.button('add to actor', {
-                        flex = 1,
-                        icon = 'plus',
-                        iconFamily = 'FontAwesome5',
-                        onClick = function()
-                           local behaviorId = entry.behaviorId
-                           self:_addBehavior(actor, behaviorId, closePopover)
-                        end,
-                    })
-                end,
+        for i = 1, #order do
+            local bp = (i == #order) and blueprint or {} -- use provided blueprint only for root component being added
+            self:send('addComponent', self.clientId, actorId, order[i], bp, {
+                interactive = true,
             })
-        end,
-    })
-
+        end
+        self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
+    end, function()
+        local behavior = self.behaviors[behaviorId]
+        if not behavior.components[actorId] then
+            return 'behavior was removed'
+        end
+        for i = #order, 1, -1 do
+            self:send('removeComponent', self.clientId, actorId, order[i])
+        end
+        self.updateCounts[actorId] = (self.updateCounts[actorId] or 1) + 1
+    end)
 end
 
 function Client:uiInspector()
@@ -223,19 +136,28 @@ function Client:uiInspector()
          if component then
             isActive = true
             for propertyName, _ in pairs(behavior.propertySpecs) do
+               -- actions to set value for each property of the behavior
                actions['set:' .. propertyName] = function(value)
-                  self:sendSetProperties(actorId, propertyName, value)
+                  behavior:sendSetProperties(actorId, propertyName, value)
                end
+               -- current value of each property of the behavior
                if behavior.getters[propertyName] then
                   properties[propertyName] = behavior.getters[propertyName](behavior, component)
                else
                   properties[propertyName] = component.properties[propertyName]
                end
             end
-            -- TODO: action to remove behavior
+            -- action to remove behavior from this actor
+            actions['remove'] = function()
+               self:_removeBehavior(actorId, component, behavior)
+            end
          else
-            -- TODO: action to add behavior
+            -- actor does not have this behavior, action to add it
+            actions['add'] = function(blueprint)
+               self:_addBehavior(actor, behavior.behaviorId, blueprint)
+            end
          end
+         
          ui.data(
             {
                name = behaviorName,
