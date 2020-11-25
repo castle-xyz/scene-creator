450a2c100b4454f5cd409708f42525c4ad06b924
Jesse Ruder <jesse@getexponent.com>
2020-11-25 13:16:13 -0800

    finish refactoring drawing subtools

---
+++ tools/draw/subtools/draw_move_subtool.lua
@@ -0,0 +1,104 @@
+local MoveTool = defineDrawSubtool {
+    category = "draw",
+    name = "move",
+}
+
+function MoveTool.handlers:addSubtool()
+    self._grabbedPaths = nil
+end
+
+function MoveTool.handlers:onTouch(component, touchData)
+    if self._grabbedPaths == nil then
+        self._grabbedPaths = {}
+
+        for i = 1, #self:drawData().pathDataList do
+            if not self:drawData().pathDataList[i].isFreehand then
+                for p = 1, 2 do
+                    local distance = math.sqrt(math.pow(touchData.touchX - self:drawData().pathDataList[i].points[p].x, 2.0) + math.pow(touchData.touchY - self:drawData().pathDataList[i].points[p].y, 2.0))
+
+                    if distance < self:drawData().scale * 0.05 then
+                        self:drawData().pathDataList[i].grabPointIndex = p
+                        table.insert(self._grabbedPaths, self:drawData().pathDataList[i])
+                        break
+                    end
+                end
+            end
+        end
+
+        for i = 1, #self._grabbedPaths do
+            self:removePathData(self._grabbedPaths[i])
+        end
+
+        if #self._grabbedPaths == 0 then
+            for i = 1, #self:drawData().pathDataList do
+                if not self:drawData().pathDataList[i].isFreehand then
+                    local pathData = self:drawData().pathDataList[i]
+                    local distance, t, subpath = pathData.tovePath:nearest(touchX, touchY, 0.5)
+                    if subpath then
+                        local pointX, pointY = subpath:position(t)
+                        self:removePathData(pathData)
+                        local touchPoint = {x = touchData.touchX, y = touchData.touchY}
+
+                        -- todo: figure out path ids here
+                        local newPathData1 = {
+                            points = {
+                                pathData.points[1],
+                                touchPoint
+                            },
+                            style = pathData.style,
+                            color = pathData.color,
+                            grabPointIndex = 2
+                        }
+
+                        local newPathData2 = {
+                            points = {
+                                touchPoint,
+                                pathData.points[2]
+                            },
+                            style = pathData.style,
+                            color = pathData.color,
+                            grabPointIndex = 1
+                        }
+
+                        table.insert(self._grabbedPaths, newPathData1)
+                        table.insert(self._grabbedPaths, newPathData2)
+
+                        break
+                    end
+                end
+            end
+        end
+
+        if #self._grabbedPaths > 0 then
+            self:drawData():resetGraphics()
+        end
+    end
+
+    for i = 1, #self._grabbedPaths do
+        self._grabbedPaths[i].points[self._grabbedPaths[i].grabPointIndex].x = touchData.roundedX
+        self._grabbedPaths[i].points[self._grabbedPaths[i].grabPointIndex].y = touchData.roundedY
+
+        self._grabbedPaths[i].tovePath = nil
+    end
+
+    if touchData.touch.released then
+        if self._grabbedPaths and #self._grabbedPaths > 0 then
+            for i = 1, #self._grabbedPaths do
+                self:addPathData(self._grabbedPaths[i])
+            end
+
+            self:drawData():resetGraphics()
+            self:drawData():resetFill()
+            self:saveDrawing("move", component)
+        end
+
+        self._grabbedPaths = nil
+        self:clearTempGraphics()
+    else
+        self:resetTempGraphics()
+
+        for i = 1, #self._grabbedPaths do
+            self:addTempPathData(self._grabbedPaths[i])
+        end
+    end
+end
