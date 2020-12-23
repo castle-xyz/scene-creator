5ba82b5a49aabee9e513d622d49618a97b842fbc
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-12-22 16:36:16 -0800

    blueprints: simplify update algo -- no more complex override tracking

--- library.lua
+++ library.lua
@@ -205,99 +205,33 @@ function Common.receivers:updateLibraryEntry(time, clientId, entryId, newEntry, 
 
     if self.clientId == clientId then
         if newEntry.entryType == "actorBlueprint" and opts.updateActors then
-            local oldBp = oldEntry.actorBlueprint
             local newBp = newEntry.actorBlueprint
-
-            local function valueEqual(value1, value2)
-                if type(value1) == "table" and type(value2) == "table" then
-                    -- Quick hack for checking table equality...
-                    return serpent.dump(value1, {sortkeys = true}) == serpent.dump(value2, {sortkeys = true})
-                else
-                    return value1 == value2
-                end
-            end
-
-            -- Collect list of changes
-            local changes = {}
-            for behaviorName, newComponentBp in pairs(newBp.components) do
-                local oldComponentBp = oldBp.components[behaviorName]
-                if oldComponentBp then -- Component already existed, check value changes
-                    for key, newValue in pairs(newComponentBp) do
-                        if not valueEqual(oldComponentBp[key], newValue) then
-                            table.insert(
-                                changes,
-                                {
-                                    changeType = "value",
-                                    behaviorName = behaviorName,
-                                    key = key,
-                                    newValue = newValue
-                                }
-                            )
-                        end
-                    end
-                    for key, oldValue in pairs(oldComponentBp) do
-                        if newComponentBp[key] == nil then -- Check for `nil`'d values skipped above
-                            table.insert(
-                                changes,
-                                {
-                                    changeType = "value",
-                                    behaviorName = behaviorName,
-                                    key = key,
-                                    newValue = nil
-                                }
-                            )
-                        end
-                    end
-                else -- Component newly added
-                    table.insert(
-                        changes,
-                        {
-                            changeType = "add component",
-                            behaviorName = behaviorName,
-                            newComponentBp = newComponentBp
-                        }
-                    )
-                end
-            end
-            for behaviorName, oldComponentBp in pairs(oldBp.components) do
-                if newBp.components[behaviorName] == nil then -- Check for removed components skipped above
-                    table.insert(
-                        changes,
-                        {
-                            changeType = "remove component",
-                            behaviorName = behaviorName
-                        }
-                    )
-                end
-            end
-
-            -- Update actors
             for actorId, actor in pairs(self.actors) do
                 if actorId ~= opts.skipActorId and actor.parentEntryId == entryId then
-                    local bp = self:blueprintActor(actorId) -- Start with old blueprint and merge changes
-                    for _, change in ipairs(changes) do
-                        local changeType, behaviorName, key = change.changeType, change.behaviorName, change.key
-                        if changeType == "value" then
-                            if bp.components[behaviorName] then
-                                if
-                                    valueEqual(
-                                        oldBp.components[behaviorName][key],
-                                        bp.components[behaviorName][key]
-                                    )
-                                 then
-                                    -- Only change value if not overridden
-                                    bp.components[behaviorName][key] = change.newValue
-                                end
-                            end
-                        elseif changeType == "add component" then
-                            bp.components[behaviorName] = change.newComponentBp
-                        elseif changeType == "remove component" then
-                            bp.components[behaviorName] = nil
+                    local oldBp = self:blueprintActor(actorId)
+                    local updateBp = util.deepCopyTable(newBp)
+                    if updateBp.components.Body then
+                        -- Keep local overrides of layout properties
+                        -- TODO(nikki): Allow pushing updates to these some times
+                        if oldBp.components.Body.x then
+                            updateBp.components.Body.x = oldBp.components.Body.x
+                        end
+                        if oldBp.components.Body.y then
+                            updateBp.components.Body.y = oldBp.components.Body.y
+                        end
+                        if oldBp.components.Body.angle then
+                            updateBp.components.Body.angle = oldBp.components.Body.angle
+                        end
+                        if oldBp.components.Body.widthScale then
+                            updateBp.components.Body.widthScale = oldBp.components.Body.widthScale
+                        end
+                        if oldBp.components.Body.heightScale then
+                            updateBp.components.Body.heightScale = oldBp.components.Body.heightScale
                         end
                     end
                     self:send("removeActor", self.clientId, actorId)
                     self:sendAddActor(
-                        bp,
+                        updateBp,
                         {
                             actorId = actorId,
                             parentEntryId = entryId,
