7501fefe44166118d893dc395f86a116ee88f3e8
Ben Roth <ben@castle.xyz>
2021-02-25 11:54:22 -0800

    add `does not equal` comparison

--- expressions/boolean.lua
+++ expressions/boolean.lua
@@ -2,6 +2,7 @@
 -- but for now we just have a helper method to resolve the existing comparison operators
 COMPARISON_OPERATORS = {
    "equal",
+   "not equal",
    "less than",
    "less or equal",
    "greater than",
@@ -10,6 +11,9 @@ COMPARISON_OPERATORS = {
 
 function Common:compare(comparison, lhs, rhs)
    if comparison == "equal" and lhs == rhs then
+      return true
+   end
+   if comparison == "not equal" and lhs ~= rhs then
       return true
    end
    if comparison == "less or equal" and lhs <= rhs then
